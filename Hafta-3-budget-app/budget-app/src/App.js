a2b82c1c3772a3e8314c667cef0a5e8fb1c4d15b
Burcu Oruç <76223789+ae-aven@users.noreply.github.com>
2022-09-11 21:26:18 +0300

    Add files via upload

---
+++ Hafta-3-budget-app/budget-app/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./App.css";
+import Header from './components/Header';
+
+const App = () => {
+  return (
+    <div className='container'>
+        <div className='app-wrapper'>
+         <Header/>
+        </div>
+    </div>
+  )
+}
+
+export default App
